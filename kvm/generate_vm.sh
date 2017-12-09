d2e7a1e9a05a3762b21ad7129a03966ab30f298a
yaohua <52devops@gmail.com>
2017-12-09 20:10:12 +0800

    fix generate_vm.sh

--- kvm/generate_vm.sh
+++ kvm/generate_vm.sh
@@ -3,16 +3,16 @@ set -e
 # 将原有raw image作为backing file利用qcow2的特性快速创建Vm
 
 # 将作为raw image的raw格式的disk(作为base的disk，通常是装完系统)
-raw_disk="Centos7_base_raw.img"
+raw_disk="ubuntu16.04_base_raw.img"
 # raw image中vm的名称
-raw_vm_name="Cen7_base_raw"
+raw_vm_name="ubuntu16_base_raw"
 # 存放raw disk的目录
 raw_dir="/vm_image/bare"
 # 存放vm disk的目录
 disk_dir='/vm_image/disk/'
 xml_dir="/vm_image/xml"
 # 产出新vm的名称前缀
-clone_disk_base_name="Cen7_lab"
+clone_disk_base_name="ubuntu16_lab"
 # 产出多少个vm
 number=4
 dst="/vm_image/disk"
@@ -27,7 +27,7 @@ do
     virsh dumpxml ${raw_vm_name}>$New_file
     sed -i "s|<name>.*</name>|<name>${clone_disk_base_name}$i</name>|g" $New_file
     sed -i "s|<uuid>.*</uuid>|<uuid>$(uuidgen)</uuid>|g" $New_file
-    sed -i "s|${disk_dir}${raw_disk}|${disk_dir}${clone_disk_base_name}$i|g" $New_file
+    sed -i "s|${raw_dir}/${raw_disk}|${disk_dir}${clone_disk_base_name}$i|g" $New_file
     sed -i "s|<driver name='qemu' type='raw'/>|<driver name='qemu' type='qcow2'/>|g" $New_file
     sed -i "s|<mac address='.*'/>|<mac address='52:54:00:fc:$(echo $RANDOM | md5sum | sed 's/\(..\)/&:/g' | cut -c1-5)'/>|g" $New_file
     virsh define ${New_file}
